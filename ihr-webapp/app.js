09dbe73099eec9b7c5daaf6abded6e1553bbf0ca
Joseph M. Honea <johonea@outlook.com>
2018-10-25 07:41:11 -0500

    Put in some code to handle the login. There are some issues, but it overall functions. Later, these routes can be moved under a separate directory

--- ihr-webapp/app.js
+++ ihr-webapp/app.js
@@ -1,65 +1,216 @@
-﻿'use strict';
-var debug = require('debug');
+﻿/* So, we are going to create the main app JavaScript file.
+ * This file will provide the following:
+ * 
+ * 1. 
+ */
+
+'use strict';
+
+/******************************************************************************
+ * Module dependencies.
+ *****************************************************************************/
+
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var passport = require('passport');
+var util = require('util');
+var config = require('./config');
+var mongoose = require('mongoose');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
 
+/******************************************************************************
+ * Set up passport in the app 
+ ******************************************************************************/
+
+//-----------------------------------------------------------------------------
+// To support persistent login sessions, Passport needs to be able to
+// serialize users into and deserialize users out of the session.  Typically,
+// this will be as simple as storing the user ID when serializing, and finding
+// the user by ID when deserializing.
+//-----------------------------------------------------------------------------
+passport.serializeUser(function (user, done) {
+    done(null, user.oid);
+});
+
+passport.deserializeUser(function (oid, done) {
+    findByOid(oid, function (err, user) {
+        done(err, user);
+    });
+});
+
+// array to hold logged in users
+var users = [];
+
+var findByOid = function (oid, fn) {
+    for (var i = 0, len = users.length; i < len; i++) {
+        var user = users[i];
+        if (user.oid === oid) {
+            return fn(null, user);
+        }
+    }
+    return fn(null, null);
+};
+
+//-----------------------------------------------------------------------------
+// Use the OIDCStrategy within Passport.
+// 
+// Strategies in passport require a `verify` function, which accepts credentials
+// (in this case, the `oid` claim in id_token), and invoke a callback to find
+// the corresponding user object.
+// 
+// The following are the accepted prototypes for the `verify` function
+// (1) function(iss, sub, done)
+// (2) function(iss, sub, profile, done)
+// (3) function(iss, sub, profile, access_token, refresh_token, done)
+// (4) function(iss, sub, profile, access_token, refresh_token, params, done)
+// (5) function(iss, sub, profile, jwtClaims, access_token, refresh_token, params, done)
+// (6) prototype (1)-(5) with an additional `req` parameter as the first parameter
+//
+// To do prototype (6), passReqToCallback must be set to true in the config.
+//-----------------------------------------------------------------------------
+passport.use(new OIDCStrategy({
+    identityMetadata: config.creds.identityMetadata,
+    clientID: config.creds.clientID,
+    responseType: config.creds.responseType,
+    responseMode: config.creds.responseMode,
+    redirectUrl: config.creds.redirectUrl,
+    allowHttpForRedirectUrl: config.creds.allowHttpForRedirectUrl,
+    clientSecret: config.creds.clientSecret,
+    validateIssuer: config.creds.validateIssuer,
+    isB2C: config.creds.isB2C,
+    issuer: config.creds.issuer,
+    passReqToCallback: config.creds.passReqToCallback,
+    scope: config.creds.scope,
+    loggingLevel: config.creds.loggingLevel,
+    nonceLifetime: config.creds.nonceLifetime,
+    nonceMaxAmount: config.creds.nonceMaxAmount,
+    useCookieInsteadOfSession: config.creds.useCookieInsteadOfSession,
+    cookieEncryptionKeys: config.creds.cookieEncryptionKeys,
+    clockSkew: config.creds.clockSkew,
+},
+    function (iss, sub, profile, accessToken, refreshToken, done) {
+        if (!profile.oid) {
+            return done(new Error("No oid found"), null);
+        }
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+            findByOid(profile.oid, function (err, user) {
+                if (err) {
+                    return done(err);
+                }
+                if (!user) {
+                    // "Auto-registration"
+                    users.push(profile);
+                    return done(null, profile);
+                }
+                return done(null, user);
+            });
+        });
+    }
+));
+
+//-----------------------------------------------------------------------------
+// Config the app, include middlewares
+//-----------------------------------------------------------------------------
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(expressSession({ secret: 'ihrwebapp021806131221', resave: true, saveUninitialized: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+
+//-----------------------------------------------------------------------------
+// Set up the route controller
+//
+// 1. For 'login' route and 'returnURL' route, use `passport.authenticate`. 
+// This way the passport middleware can redirect the user to login page, receive
+// id_token etc from returnURL.
+//
+// 2. For the routes you want to check if user is already logged in, use 
+// `ensureAuthenticated`. It checks if there is an user stored in session, if not
+// it will call `passport.authenticate` to ask for user to log in.
+//-----------------------------------------------------------------------------
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/login');
+};
+
+app.get('/', function (req, res) {
+    res.render('index', { user: req.user });
 });
 
-// error handlers
+// '/account' is only available to logged in user
+app.get('/dashboard', ensureAuthenticated, function (req, res) {
+    res.render('dashboard', { user: req.user });
+});
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+app.get('/login',
+    function (req, res, next) {
+        passport.authenticate('azuread-openidconnect',
+            {
+                response: res,                      // required
+                resourceURL: config.resourceURL,    // optional. Provide a value if you want to specify the resource.
+                customState: 'my_state',            // optional. Provide a value if you want to provide custom state value.
+                failureRedirect: '/'
+            }
+        )(req, res, next);
+    },
+    function (req, res) {
+        res.redirect('/');
     });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+// 'GET returnURL'
+// `passport.authenticate` will try to authenticate the content returned in
+// query (such as authorization code). If authentication fails, user will be
+// redirected to '/' (home page); otherwise, it passes to the next middleware.
+app.get('/login/auth/openid/return',
+    function (req, res, next) {
+        passport.authenticate('azuread-openidconnect',
+            {
+                response: res,                      // required
+                failureRedirect: '/'
+            }
+        )(req, res, next);
+    },
+    function (req, res) {
+        res.redirect('/');
+    });
+
+// 'POST returnURL'
+// `passport.authenticate` will try to authenticate the content returned in
+// body (such as authorization code). If authentication fails, user will be
+// redirected to '/' (home page); otherwise, it passes to the next middleware.
+app.post('/login/auth/openid/return',
+    function (req, res, next) {
+        passport.authenticate('azuread-openidconnect',
+            {
+                response: res,                      // required
+                failureRedirect: '/'
+            }
+        )(req, res, next);
+    },
+    function (req, res) {
+        res.redirect('/');
+    });
+
+// 'logout' route, logout from passport, and destroy the session with AAD.
+app.get('/logout', function (req, res) {
+    req.session.destroy(function (err) {
+        req.logOut();
+        res.redirect(config.destroySessionUrl);
     });
 });
 
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function () {
-    debug('Express server listening on port ' + server.address().port);
-});
+app.listen(3000);
