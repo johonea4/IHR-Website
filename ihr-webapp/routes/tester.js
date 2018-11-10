9525487393cdd5b442f27190a9d7eb4b474e3b44
ilin930 <ilin930@gmail.com>
2018-11-10 13:30:39 -0800

    test1 with FHIR compliance JSON test to add patient.

--- ihr-webapp/routes/tester.js
+++ ihr-webapp/routes/tester.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var fhirUtil = require('../utils/fhirUtil');
 var dbutil = require('../utils/dbutil');
 
+var fhirtestUtil = new fhirUtil("http://hapi.fhir.org/baseDstu3");
+
 router.get('/', function (req, res) {
     res.render('UserDataEntry', { formAction: "tester/submit" });
     console.log("Testing /");
@@ -24,6 +26,7 @@ router.post('/submit', function (req, res) {
 
 router.get('/test1', function (req, res) {
     console.log("Testing /test1");
+    fhirtestUtil.addPatient();
 });
 
 router.get('/test2', function (req, res) {
