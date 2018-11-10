9e462baf5e5655e584240063749d5931e9d318f2
ilin930 <ilin930@gmail.com>
2018-11-10 13:31:38 -0800

    adding addition changes.

--- ihr-webapp/utils/fhirUtil.js
+++ ihr-webapp/utils/fhirUtil.js
@@ -50,21 +50,53 @@ module.exports = class fhirUtil {
 
         return results;
     }
+    addPatient(){
+        var entry = {
+            "name": [{
+                "use"   : "official",
+                "text"  : "Eric Lin",
+                "family": "Lin",
+                "given" : "Eric"
+                }],
+            "gender": "male",
+            "birthDate": "1970-01-01",
+            "resourceType": 'Patient',
+            "status": "active",
+            "medicationCodeableConcept": {
+                "coding": [{
+                    "system": "http://snomed.info/ct",
+                    "code": "318632005",
+                    "display": "carvedilol 25 mg"
+                }],
+                "text": "carvedilol 25 mg"
+            },
+            "subject": {"reference": "Patient/23129"},
+            "taken": "y",
+            "dosage": [{
+                "text": "twice daily",
+                "timing": {
+                    "repeat": {
+                    "frequency": 2,
+                    "period": 1,
+                    "periodUnit": "d"
+                    }},
+                "doseQuantity": {
+                    "value": 1,
+                    "unit": "tablet",
+                    "system": "http://snomed.info/ct",
+                    "code": "428673006"
+                }
+            }]
+        } 
+        console.log("need to see this first...");
+        this.client.create(
+            entry,
+            function(entry){
+                console.log("are we here?");
+                console.log(entry.id);
+            },
+            function(error){console.error(error);}
+        )   
+    }       
 }
 
-
-// Will clean up later. for now, just kept below for reference...
-
-// var resource = {
-//     resourceType: 'Patient'   
-//   };
-//   var fhir = new Fhir();
-//   var xml = fhir.objToXml(resource);
-//   var json = fhir.xmlToJson(xml);
-//   var obj = fhir.xmlToObj(xml);
-//   var results = fhir.validate(xml, { errorOnUnexpected: true });
-//   results = fhir.validate(obj, {});
-//   console.log("this is result: ", results)
-
-// FhirContext ctx = FhirContext.forDstu3();
-// client = ctx.newRestfulGenericClient(baseUrl);
