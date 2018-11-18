7012534532ab147addc8e44a446a152e872b8735
ilin930 <ilin930@gmail.com>
2018-11-17 22:47:29 -0800

    joseph updated with promise for getPatientMedications.

--- ihr-webapp/utils/fhirUtil.js
+++ ihr-webapp/utils/fhirUtil.js
@@ -77,5 +77,74 @@ module.exports = class fhirUtil {
                 }
             });
     }
+    async addPatient() {
+        var entry = {
+            name: [{
+                use: "official",
+                text: "Eric Lin",
+                family: "Lin",
+                given: "Eric"
+            }],
+            gender: "male",
+            birthDate: "1970-01-01",
+            resourceType: 'Patient',
+            status: "active"
+        };
+        console.log("need to see this first...");
+        try {
+            let res = await this.client.create({ type: 'Patient', resource: entry });
+            console.log("are we here?");
+            console.log(res);
+        } catch (err) {
+            errFunc(err);
+        }   
+    }
+    async addPatientMedication( patientID ) {
+
+        var entry = {
+            resourceType: "MedicationStatement",
+            //id: "23201",
+            meta: {
+                versionId: "1",
+                lastUpdated: "2018-10-01T07:31:49.588+00:00"
+            },
+            status: "active",
+            medicationCodeableConcept: {
+                coding: [{
+                    system: "http://snomed.info/ct",
+                    code: "321323009",
+                    display: "buspirone 5mg"
+                }],
+                text: "buspirone 5mg"
+            },
+            subject: {
+                reference: "Patient/" + patientID
+            },
+            effectiveDateTime: "2018-05-12T04:04:13.435+00:00",
+            taken: "y",
+            dosage: [{
+                    text: "3 times daily",
+                    timing: {
+                        repeat: {
+                        frequency: 3,
+                        period: 1,
+                        periodUnit: "d"
+                        }
+                    },
+                    doseQuantity: {
+                        value: 1,
+                        unit: "tablet",
+                        system: "http://snomed.info/ct",
+                        code: "428673006"
+                    }
+            }]
+        }
+        try {
+            let res = await this.client.create({ type: 'MedicationStatement', resource: entry });
+            console.log(res);
+        } catch (err) {
+            errFunc(err);
+        }  
+    }
 }
 
