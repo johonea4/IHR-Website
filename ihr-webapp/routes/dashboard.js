43cb3cddd50404d80768bd518ca96cc7f9c44b17
Dgizas <dgizas3@gatech.edu>
2018-11-30 22:15:33 -0600

    Chart Creator update

--- ihr-webapp/routes/dashboard.js
+++ ihr-webapp/routes/dashboard.js
@@ -1,9 +1,25 @@
 ﻿var express = require('express');
 
 var router = express.Router();
+var dbUtil = require('../utils/dbutil');
+
 
 router.get('/', function (req, res) {
-    res.render('dashboard', { user: req.user });
+    async function FetchWrapper()
+    {
+        await FetchData(req, res);    
+    }
+
+    FetchWrapper();
 });
+
+async function FetchData(req, res)
+{
+    dbUtil.getPatient(req.user.oid).then(async function(p)
+    {
+        var medData = JSON.stringify(p);
+        res.render('dashboard', { user: req.user, chartData: medData });
+    });    
+}
 
 exports.router = router;
